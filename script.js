57628e0064edbf34711aadee6dca6ca9dd8692b6
Horlar9475 <festusakinboye@gmail.com>
2025-05-19 20:23:18 +0100

    Add project page and update functionality

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
-// display about page
-const aboutPage = document.querySelector('.aboutClick')
-const myAbout = document.querySelector('.myAbout')
-aboutPage.addEventListener('click' , ()=>{
-    myAbout.style.display = 'flex'
-    if(sideMenu.style.position == 'absolute'){
-        sideMenu.style.display = 'none'
-        burgermenu.style.display = 'none'
-        closeAbout.style.display = 'grid'
-    }
-})
-const bkHome = document.querySelector('.homePage')
-bkHome.addEventListener('click' , ()=>{
-    myAbout.style.display = 'none'
-    if(sideMenu.style.position == 'absolute'){
-        sideMenu.style.display = 'none'
-    }
-})
-const mailMe = document.querySelector('.myMailContact')
-mailMe.addEventListener('click' , ()=>{
-    location.href = 'mailto:festusakinboye@gmail.com'
-})
+// Fetching all needed variable
+const myHome = document.querySelector('.homePage')
+const aboutClick = document.querySelector('.aboutClick')
+const projectsClick = document.querySelector('.projectsClick')
+// Getting the pages
+const firstPage = document.querySelector('.frontNameDiv')
+const aboutPage = document.querySelector('.myAbout')
+const myProjects = document.querySelector('.allProjects')
+//Getting my dashboard menu
+const myDashboard = document.querySelector('.sec1')
+//Getting the Hamburger menus
+const hamburger = document.querySelector('.hamburger')
+const closePage = document.querySelectorAll('.closeAbout')
 
-// hamburger menu
-const burgermenu = document.querySelector('.hamburger')
-const closeAbout = document.querySelector('.closeAbout')
-const sideMenu = document.querySelector('.sec1')
-burgermenu.addEventListener('click' , ()=>{
-    sideMenu.style.display = (sideMenu.style.display == 'flex') ? 'none' : 'flex'
-    sideMenu.style.position = 'absolute'
-    sideMenu.style.right = '0px'
+// Adding the event listeners to each pages
+myHome.addEventListener('click',()=>{
+    firstPage.style.display = 'flex'
+    aboutPage.style.display = 'none'
+    myProjects.style.display = 'none'
 })
-closeAbout.addEventListener('click' , ()=>{
-    myAbout.style.display = 'none'
-    burgermenu.style.display = 'block'
+aboutClick.addEventListener('click',()=>{
+    aboutPage.style.display = 'flex'
+    firstPage.style.display = 'none'
+    myProjects.style.display = 'none'
+    if(myDashboard.style.position == 'absolute'){
+        myDashboard.style.display = 'none'
+        hamburger.style.display = 'none'
+        const clAbt = document.getElementById('clAbt')
+        clAbt.style.display = 'grid'
+    }
+})
+projectsClick.addEventListener('click', ()=>{
+    myProjects.style.display = 'flex'
+    firstPage.style.display = 'none'
+    aboutPage.style.display = 'none'
+    if(myDashboard.style.position == 'absolute'){
+        myDashboard.style.display = 'none'
+        hamburger.style.display = 'none'
+        const clPr = document.getElementById('clPr')
+        clPr.style.display = 'grid'
+    }
+})
+// The Hamburger Menu
+hamburger.addEventListener('click', ()=>{
+    myDashboard.style.display = (myDashboard.style.display == 'flex') ? 'none' : 'flex'
+    myDashboard.style.position = 'absolute'
+    myDashboard.style.right = '0%'
+
+})
+// Page close menu
+closePage.forEach((menu)=>{
+    menu.addEventListener('click' , ()=>{
+        firstPage.style.display = 'flex'
+        myProjects.style.display = 'none'
+        aboutPage.style.display = 'none'
+        hamburger.style.display = 'block'
+    })
 })
